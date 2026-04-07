00e764de07783a6448340ea000afc2992f39da4b
omkar <omkarchinnam01@gmail.com>
2026-04-07 22:10:49 +0530

    Collections Generic type

--- Collections/ArrayListGeneric.java
+++ Collections/ArrayListGeneric.java
@@ -6,7 +6,9 @@ public class ArrayListGeneric {
 
         // Creating a Collection that can store ONLY Integer values
         // <Integer> is called a "generic type"
+//        Generics in Java provide type safety, eliminate casting, and help detect errors at compile time instead of runtime.
         // It provides type safety (only Integer objects are allowed)
+        // List allows duplicate elements and maintains insertion order
         Collection<Integer> list = new ArrayList<Integer>();
 
         // Adding elements into the collection
@@ -14,6 +16,10 @@ public class ArrayListGeneric {
         list.add(10);   // int → automatically converted to Integer (autoboxing)
         list.add(62);
         list.add(23);
+        list.add(10);
+        list.add(78);
+        list.add(65);
+        list.add(62);
 
         // list.add("hello"); ❌ Compile-time error (Type safety enforced)
 
